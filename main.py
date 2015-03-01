5854a9954495b5e72bdc90b468b02484dd0a9469
Daniel Ladenheim <ladenheim@gmail.com>
2015-03-01 15:44:02 -0500

    Add concept of city and have search export json serialized string.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import json
 import webapp2
 
 from google.appengine.ext import ndb
 
+class Address(ndb.Model):
+	address = ndb.StringProperty()
+	city = ndb.StringProperty()
+
 class Bar(ndb.Model):
 	name = ndb.StringProperty()
 	teams = ndb.StringProperty(repeated=True)
+	address = ndb.StringProperty()
+	city = ndb.StringProperty()
 
 
 class MainPage(webapp2.RequestHandler):
@@ -17,30 +24,49 @@ class SearchHandler(webapp2.RequestHandler):
 	def get(self):
 		self.response.headers['Content-Type'] = 'text/plain'
 		search_val = self.request.get("value").lower()
-		bars = Bar.query(
-			ndb.OR(Bar.name == search_val, Bar.teams == search_val)
-			).fetch()
+		city = self.request.get("city").lower()
+		bars = []
+		if city:
+			bars = Bar.query(
+				ndb.AND(
+					ndb.OR(Bar.name == search_val, Bar.teams == search_val),
+					Bar.city == city)
+				).fetch()
+		else:
+			bars = Bar.query(
+				ndb.OR(
+					Bar.name == search_val, 
+					Bar.teams == search_val)
+				).fetch()
 		if not bars:
-			self.response.out.write("Could not find results for: " + search_val)
+			self.response.out.write('')
 			return
+
+		bars_json = []
 		for bar in bars:
-			info = bar.name.title() + ":" 
-			info += ','.join(bar.teams).title()
-			self.response.out.write(info)
+			bars_json.append({
+				'name': bar.name.title(),
+				'teams': [t.title() for t in bar.teams],
+				'city': bar.city.title(),
+				'address': bar.address.title()})
+		self.response.out.write(json.dumps(bars_json))
 
 
 class Populate(webapp2.RequestHandler):
 	def get(self):
-		bar_name = self.request.get("bar").lower().lstrip().rstrip()
-		bar_teams = self.request.get("teams")
+		name = self.request.get("bar").lower().lstrip().rstrip()
+		teams_str = self.request.get("teams")
+		address = self.request.get("address").lower().lstrip().rstrip()
+		city = self.request.get("city").lower().lstrip().rstrip()
+
 		# Do not add the same bar twice.
-		bar = Bar.query(Bar.name == bar_name).fetch()
+		bar = Bar.query(ndb.AND(Bar.name == name, Bar.city == city)).fetch()
 		teams = []
-		for team in bar_teams.split(','):
+		for team in teams_str.split(','):
 			if team and team.lstrip().rstrip():
 				teams.append(team.lower().lstrip().rstrip())
 		if not bar:
-			bar = Bar(name=bar_name, teams=teams)
+			bar = Bar(name=name, teams=teams, address=address, city=city)
 		else:
 			bar = bar[0]
 			bar.teams = list(set(teams) | set(bar.teams))
@@ -50,8 +76,7 @@ class Populate(webapp2.RequestHandler):
 class Reset(webapp2.RequestHandler):
 	def get(self):
 		bars = Bar.query().fetch()
-		for bar in bars:
-			bar.key.delete()
+		ndb.delete_multi([bar.key for bar in bars])
 
 
 application = webapp2.WSGIApplication([
